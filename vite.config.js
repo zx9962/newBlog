36936f0222d193a23ac2c410a4f21f8b702b8481
15539927692 <zy2905593394@163.com>
2025-03-19 16:28:00 +0800

    66

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base:'/docs/'
-})
+  base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        sanitizeFileName: false, // 禁用文件名清理
+      },
+    },
+  },
+});
